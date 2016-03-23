eb416cea6402332c3aea318fecce8dd16d3bc01e
Zak Angelle <zaknotzak@gmail.com>
2016-03-23 12:48:17 -0500

    Cleaned up parser

--- server/schedule.js
+++ server/schedule.js
@@ -42,37 +42,38 @@ function convertScheduleToJson(schedule)
     return $schedule.text().trim();
   }])
   .then(function(items) {
-    var games = [];
-
-    var timesToSplice = items.length / 5;
-    for (var i=0; i < timesToSplice; i++) {
-      games.push(items.splice(0, 5));
-    }
-
-    var gamesFormatted = [];
-    games.forEach(function(g) {
-      var game = {};
-
-      // TODO - Figure out a better way to infer the year
-      game.date = g[0] + '/' + moment().format('YYYY');
-      game.time = g[1];
-      game.away_team = g[2].replace(/\s\d+/g, '').trim();
-      game.home_team = g[3].replace(/\s\d+/g, '').trim();
-
-      var awayScore = g[2].match(/\d+$/);
-      var homeScore = g[3].match(/\d+$/);
-
-      if (awayScore && !isNaN(awayScore[0])) {
-        game.away_score = parseInt(awayScore[0]);
-        game.home_score = parseInt(homeScore[0]);
-      }
-
-      game.location = g[4];
-
-      gamesFormatted.push(game);
-    });
-
-    return gamesFormatted;
+    return getGamesFromItems(items).map(g => formatGame(g));
   });
 }
 
+function getGamesFromItems(items) {
+  var games = [];
+
+  var timesToSplice = items.length / 5;
+  for (var i=0; i < timesToSplice; i++) {
+    games.push(items.splice(0, 5));
+  }
+
+  return games;
+}
+
+function formatGame(g) {
+  var game = {};
+
+  game.date = g[0] + '/' + moment().format('YYYY');
+  game.time = g[1];
+  game.away_team = g[2].replace(/\s\d+/g, '').trim();
+  game.home_team = g[3].replace(/\s\d+/g, '').trim();
+
+  var awayScore = g[2].match(/\d+$/);
+  var homeScore = g[3].match(/\d+$/);
+
+  if (awayScore && !isNaN(awayScore[0])) {
+    game.away_score = parseInt(awayScore[0]);
+    game.home_score = parseInt(homeScore[0]);
+  }
+
+  game.location = g[4];
+
+  return game;
+}
