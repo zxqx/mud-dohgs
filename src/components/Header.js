48ee5b612a36d70c46ac8bbe68ae865f34113783
Zak Angelle <zaknotzak@gmail.com>
2016-03-23 14:35:19 -0500

    Removed bootstrap dep

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Navbar, NavbarBrand } from 'react-bootstrap';
 import CSSModules from 'react-css-modules';
 import styles from '../style/header.css';
 
@@ -10,12 +9,12 @@ export default class Header extends Component {
     const { styles } = this.props;
 
     return (
-      <Navbar className={styles.navbar}>
-        <NavbarBrand styleName='navbar-brand'>
+      <div className={styles.navbar}>
+        <div styleName='navbar-brand'>
           <span styleName='navbar-team'>Mud Dohgs</span>
           <span styleName='navbar-section'>Schedule</span>
-        </NavbarBrand>
-      </Navbar>
+        </div>
+      </div>
     );
   }
 }
