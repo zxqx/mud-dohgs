2577a26c226d700a3a88b40ead3a95cfc9c17b22
Zak Angelle <zaknotzak@gmail.com>
2016-03-24 11:40:28 -0500

    Added get and set schedule url methods

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,5 +1,8 @@
+import Firebase from 'firebase';
 import { camelizeKeys } from 'humps';
 import moment from 'moment';
+
+const REMOTE_DATA_STORE_ROOT = 'https://mud-dohgs.firebaseio.com';
 
 export async function fetchGameList() {
   const res = await fetch('/api/schedule');
@@ -14,4 +17,16 @@ export async function fetchGameList() {
   data.data.forEach(d => d.date = moment(d.date, 'ddd MM/DD/YYYY'));
 
   return data;
+}
+
+export async function updateScheduleUrl(url) {
+  const ref = new Firebase(`${REMOTE_DATA_STORE_ROOT}/schedule-url`);
+  ref.child('url').set(url);
+}
+
+export async function getScheduleUrl() {
+  const ref = new Firebase(`${REMOTE_DATA_STORE_ROOT}/schedule-url`);
+
+  ref.on('value', snapshot => console.log(snapshot.val()),
+    error => console.log(error));
 }
