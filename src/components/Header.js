8627af97a967609b175e632ab67e486a541bbbbf
Zak Angelle <zaknotzak@gmail.com>
2017-03-23 17:52:19 -0500

    Better mobile styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import HamburgerMenu from 'react-hamburger-menu';
-import MediaQuery from 'react-responsive';
 import cssModules from 'react-css-modules';
 import headerStyles from '../style/header.css';
 
@@ -31,73 +30,13 @@ export default class Header extends Component {
 
     return (
       <div>
-        <MediaQuery query='(max-width: 767px)'>
-          {this.state.open &&
-            <div>
-              <div styleName='mobile-menu-container' ref='mobileMenuContainer'>
-                <div styleName='mobile-menu-icon-inner'>
-                  <HamburgerMenu
-                    isOpen={this.state.open}
-                    menuClicked={this.toggleMenu.bind(this)}
-                    width={18}
-                    height={15}
-                    strokeWidth={2}
-                    rotate={0}
-                    color='#333'
-                    borderRadius={0}
-                    animationDuration={0.5}
-                  />
-                </div>
-
-                <h2 styleName='mobile-menu-header'>Mud Dohgs</h2>
-
-                <nav styleName='mobile-menu'>
-                  <ul>
-                    <li>
-                      <Link
-                        to='/'
-                        onClick={this.toggleMenu.bind(this)}
-                      >
-                        Schedule
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to='/standings'
-                        onClick={this.toggleMenu.bind(this)}
-                      >
-                        Standings
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-
-              <div
-                styleName='overlay'
-                ref='overlay'
-                onClick={this.toggleMenu.bind(this)}
-              >
-              </div>
-            </div>
-          }
-        </MediaQuery>
-
-        <div className={styles.navbar}>
-          <div styleName='navbar-brand'>
-            <Link to='/'>
-              <span styleName='navbar-team'>Mud Dohgs</span>
-            </Link>
-
-            <MediaQuery query='(min-width: 768px)'>
-              <span styleName='navbar-section'>
-                <Link to='/' styleName='navbar-link'>Schedule</Link>
-                <Link to='/standings' styleName='navbar-link'>Standings</Link>
-              </span>
-            </MediaQuery>
-
-            <MediaQuery query='(max-width: 767px)'>
-              <div styleName='mobile-menu-icon'>
+        {this.state.open &&
+          <div>
+            <div
+              styleName='mobile-menu-container'
+              ref='mobileMenuContainer'
+            >
+              <div styleName='mobile-menu-icon-inner'>
                 <HamburgerMenu
                   isOpen={this.state.open}
                   menuClicked={this.toggleMenu.bind(this)}
@@ -105,12 +44,69 @@ export default class Header extends Component {
                   height={15}
                   strokeWidth={2}
                   rotate={0}
-                  color='#bbb'
+                  color='#333'
                   borderRadius={0}
                   animationDuration={0.5}
                 />
               </div>
-            </MediaQuery>
+
+              <h2 styleName='mobile-menu-header'>Mud Dohgs</h2>
+
+              <nav styleName='mobile-menu'>
+                <ul>
+                  <li>
+                    <Link
+                      to='/'
+                      onClick={this.toggleMenu.bind(this)}
+                    >
+                      Schedule
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to='/standings'
+                      onClick={this.toggleMenu.bind(this)}
+                    >
+                      Standings
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div
+              styleName='overlay'
+              ref='overlay'
+              onClick={this.toggleMenu.bind(this)}
+            >
+            </div>
+          </div>
+        }
+
+        <div className={styles.navbar}>
+          <div styleName='navbar-brand'>
+            <Link to='/'>
+              <span styleName='navbar-team'>Mud Dohgs</span>
+            </Link>
+
+            <span styleName='navbar-section'>
+              <Link to='/' styleName='navbar-link'>Schedule</Link>
+              <Link to='/standings' styleName='navbar-link'>Standings</Link>
+            </span>
+
+            <div styleName='mobile-menu-icon'>
+              <HamburgerMenu
+                isOpen={this.state.open}
+                menuClicked={this.toggleMenu.bind(this)}
+                width={18}
+                height={15}
+                strokeWidth={2}
+                rotate={0}
+                color='#bbb'
+                borderRadius={0}
+                animationDuration={0.5}
+              />
+            </div>
           </div>
         </div>
       </div>
